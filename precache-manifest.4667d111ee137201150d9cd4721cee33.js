cc07a0fcaf1a98ce716f4a8305c0c62f037e6fe6
Muhammad Haseeb <haseebmuhammad029@gmail.com>
2020-11-07 02:55:27 +0500

    Updates

---
+++ precache-manifest.4667d111ee137201150d9cd4721cee33.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758b3b850abcb1e6ced59f9e459a8391",
+    "url": "/React-Restaurant-App/index.html"
+  },
+  {
+    "revision": "fc75ed4d5dad79af7cf7",
+    "url": "/React-Restaurant-App/static/css/2.49d7573d.chunk.css"
+  },
+  {
+    "revision": "92093c3af89bf73ac842",
+    "url": "/React-Restaurant-App/static/css/main.bc2a2813.chunk.css"
+  },
+  {
+    "revision": "fc75ed4d5dad79af7cf7",
+    "url": "/React-Restaurant-App/static/js/2.878d03d7.chunk.js"
+  },
+  {
+    "revision": "64d2be57179ef309ad7462ca9b94dadd",
+    "url": "/React-Restaurant-App/static/js/2.878d03d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92093c3af89bf73ac842",
+    "url": "/React-Restaurant-App/static/js/main.76e6d5aa.chunk.js"
+  },
+  {
+    "revision": "4047304dc498f224deb9",
+    "url": "/React-Restaurant-App/static/js/runtime-main.befcc353.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Restaurant-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Restaurant-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Restaurant-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Restaurant-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Restaurant-App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
